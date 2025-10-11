dae356070188db833bb2fb15f0737ae1ab174262
ShadowCone <git@shadowcone.com>
2025-10-11 16:20:02 +0200

    fixup training ctfconfig

--- ataka/ctfconfig/training.py
+++ ataka/ctfconfig/training.py
@@ -20,7 +20,7 @@ STATIC_EXCLUSIONS = {
     "TODO: FILL-IN-HERE"
 }
 
-ROUND_TIME = 120
+ROUND_TIME = 60
 
 # format: regex, group where group 0 means the whole regex
 FLAG_REGEX = r"shadow\{[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\}", 0
@@ -43,11 +43,14 @@ FLAGID_URL = "http://ctf.h4xx.eu/flagids"
 def get_targets():
     try:
         flag_ids = requests.get(FLAGID_URL, timeout=1).json()
-        services = set([x for x in team.keys() for team in flag_ids.values()])
+        services = set([x for team in flag_ids.values() for x in team.keys()])
+        flag_ids = {service: {f"10.99.{i}.1": flag_ids[i][service] if i in flag_ids and service in flag_ids[i] else [] for i in flag_ids.keys()} for service in services}
     except Exception as e:
         print(f"Got error during flagid checking: {e}")
         flag_ids = {}
-        services = []
+        services = set()
+
+    services |= {"doeboard"}
 
     ## A generic solution for just a single vulnbox:
     default_targets = {service: {f"10.99.{i}.1": [] for i in list(range(1,8)) + [99]} for service in services}
@@ -60,24 +63,24 @@ def get_targets():
             }
             for ip, ip_info in (default_targets[service] | service_info).items()
         ]
-        for service, service_info in ({service: [] for service in services} | flag_ids).items()
+        for service, service_info in ({service: {} for service in services} | flag_ids).items()
     }
 
     return targets
 
 
 def submit_flags(flags):
-    resp = requests.put(
-        SUBMIT_URL, headers={"X-Team-Token": TEAM_TOKEN}, json=flags
+    resp = requests.post(
+        SUBMIT_URL, headers={"X-Token": TEAM_TOKEN}, json=flags
     ).json()
 
     results = []
     for flag in flags:
-        if flag not in flag_resp:
+        if flag not in resp:
             status = FlagStatus.ERROR
             print(f"Error while flag submission: got no response for flag {flag}")
         else:
-            msg = flag_resp[flag]
+            msg = resp[flag]
             match msg:
                 case "ok":
                     status = FlagStatus.OK
@@ -88,7 +91,7 @@ def submit_flags(flags):
                 case "ownflag":
                     status = FlagStatus.OWNFLAG
                 case "invalid":
-                    status = Flagstatus.INVALID
+                    status = FlagStatus.INVALID
                 case _:
                     status = FlagStatus.ERROR
                     print(f"Error while flag submission: {msg}")
